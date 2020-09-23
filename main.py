1b4464c88e2ada27395579dde62911c0025ebe5e
Giotto12138 <705808548@qq.com>
2020-09-22 21:44:40 -0400

    initial

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from google.cloud import datastore
 
 
@@ -8,23 +8,33 @@ DS = datastore.Client()
 EVENT = 'Event' # Name of the event table, can be anything you like.
 ROOT = DS.key('Entities', 'root') # Name of root key, can be anything.
 
-# get function
+# main page
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 """
     post function
     variables: {name} {date_str}, 
-    add to google cloud datastore
+    add new event to google cloud datastore
 """
-@app.route('/')
-def put_event(name, date_str):
+@app.route("/event", methods = ["POST"])
+def post_event(name, date_str):
     entity = datastore.Entity(key=DS.key(EVENT, parent=ROOT))
     entity.update({'name': name, 'date': date_str})
     DS.put(entity)
+    
 
-
+"""
+    get function
+    get all the existing events from google cloud datastore
+"""
+@app.route("/events", methods = ["GET"])
+def get_events():
+    
+    for event in DS.query(kind = EVENT, ancestor = ROOT).fetch():
+        return jsonify({"events":[{"name": event["name"], "date": event["date"], "id": event.id}], "error": "getError"})
 
 
 if __name__ == '__main__':
