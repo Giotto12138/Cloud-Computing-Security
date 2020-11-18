73c212b13e6bb69c98d486827120eaeb3d95898e
Giotto12138 <705808548@qq.com>
2020-11-17 21:03:44 -0500

    delete shell files

--- lab2/env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export GOOGLE_APPLICATION_CREDENTIALS="D:\JHU\courses\20fall\cloud security\qingshan-cloud-security-513ba52b1c96.json"
-export GAE_ENV="localdev"
-
-# windows env command
-# $env:GOOGLE_APPLICATION_CREDENTIALS="D:\JHU\courses\20fall\cloud security\qingshan-cloud-security-513ba52b1c96.json"
-
-python main.py
